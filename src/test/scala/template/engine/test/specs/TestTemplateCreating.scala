e08b6fb58789315230da52acb04574fbb9fe8928
Mads Hartmann <mads379@gmail.com>
2010-07-21 11:54:49 +0200

    Forgot to add the new test case and it's resource.

---
+++ src/test/scala/template/engine/test/specs/TestTemplateCreating.scala
@@ -0,0 +1,20 @@
+package template.engine.test.specs
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import java.io.File
+import template.engine._
+import template.engine.test
+import net.liftweb.common.{Box, Empty, Failure, Full}
+
+class TestTemplateCreating extends FlatSpec with ShouldMatchers {
+  
+  "Snippet template" should "be created" in {
+    TestTemplateProcessor.processInput("create snippet Name my.package")
+    val f = new File("src/test/output/snippet.scala")
+    f.exists should be === true
+    f.delete
+  }
+  
+  
+}
