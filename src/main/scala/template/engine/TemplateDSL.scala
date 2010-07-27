088b7fa874922589ecc7078ef455890b36f46fe4
Mads Hartmann <mads379@gmail.com>
2010-07-27 13:34:58 +0200

    Added a question at the top. Hoping someone on twitter can help

--- src/main/scala/template/engine/TemplateDSL.scala
+++ src/main/scala/template/engine/TemplateDSL.scala
@@ -2,7 +2,13 @@
 | All the classes in this file are used to built the Tempalte DSL. That means
 | stuff like: 
 |
-|  injectContentsOfFile "path/to/file" into "myfile.txt" atPoint "MyPoint" inTemplate "empty"
+|  injectContentsOfFile "path/to/file" into "myfile.txt" atPoint "MyPoint"
+|
+| HOWEVER currently I have to write it like this: 
+|
+| injectContentsOfFile("src/test/resources/user_inject_Model_Point.txt").into("src/test/resources/model.txt").at("Point")
+|
+| What do I have to do to be able to drop () and dots?
 |
 |==============================================================================================*/
 
@@ -23,28 +29,13 @@ trait InjectableIntoFileTrait {
   val injectable: InjectableTrait
   val pathToTemplate: String
   
-  def at(point: String) = InjectableIntoFileWithPoint(this, point) 
+  def at(point: String) = TemplateInjection(injectable.pathToFile, pathToTemplate, point, injectable.dependentTemplate)
 }
 
-trait InjectableIntoFileWithPointTrait {
-  
-  val injectableIntoFile: InjectableIntoFileTrait
-  val point: String
-  
-  def inTemplate(template: Template) = 
-    TemplateInjection(
-      injectableIntoFile.injectable.pathToFile, 
-      injectableIntoFile.pathToTemplate, 
-      point, 
-      template, 
-      injectableIntoFile.injectable.dependentTemplate)
-}
 
 case class Injectable(pathToFile: String, dependentTemplate: Template) extends InjectableTrait
 
 case class InjectableIntoFile(injectable: InjectableTrait, pathToTemplate: String) extends InjectableIntoFileTrait
-
-case class InjectableIntoFileWithPoint(injectableIntoFile: InjectableIntoFileTrait, point: String) extends InjectableIntoFileWithPointTrait
 
 /**
 * This represents something that you want to inject into at file. You are not meant to create instances
@@ -53,11 +44,10 @@ case class InjectableIntoFileWithPoint(injectableIntoFile: InjectableIntoFileTra
 * @param  file              The path to the file whose content you want to inject
 * @param  into              The path to the file you want to inject code into
 * @param  atPoint           The name of the injection point of the file.
-* @param  template          The tempalte whose file you want to inject somethign into
 * @param  dependentTemplate The template that wants to inject something into it. 
 * 
 */
-case class TemplateInjection(file: String, into: String, point: String, template: Template, dependentTemplate: Template) {
+case class TemplateInjection(file: String, into: String, point: String, dependentTemplate: Template) {
   
   dependentTemplate.addInjection(this)
   
