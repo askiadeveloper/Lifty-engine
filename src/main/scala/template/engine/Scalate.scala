88a912c86179156dda72a3d09b3f2bc99c22d030
Mads Hartmann <mads379@gmail.com>
2010-08-03 16:22:36 +0200

    It now states the name of the file (without the .ssp extension)

--- src/main/scala/template/engine/Scalate.scala
+++ src/main/scala/template/engine/Scalate.scala
@@ -61,13 +61,13 @@ case class Scalate(template: Template with Create, argumentResults: List[Argumen
         
     val messageUnusedInjections = if (unusedInjections.size > 0) {
       val stroke = "-----------------------------------------------"
-      "\n\nThe template was not able inject the following: %s\n%s\n%s".format(
+      "\n\nThe template was not able inject the following: \n%s\n%s\n%s".format(
         stroke,
         unusedInjections.map{
         injection => 
           "\n\nCode:\n%s\n\ninto: %s at %s".format(
             processTemplateInMemory(new File(injection.file)),
-            injection.into,
+            injection.into.split("\\.").first,
             injection.point
           )
       }.mkString("\n\n"),
