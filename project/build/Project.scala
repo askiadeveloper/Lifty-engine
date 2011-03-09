1970ec5a07f3d256c45c8243122ad2a8dd4c0d6b
Mads Hartmann <mads379@gmail.com>
2011-03-09 15:06:37 +0100

    fixed tabs

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,17 +1,18 @@
 import sbt._
 
-class SimpleBuildToolTemplateEngine(info: ProjectInfo) extends ParentProject(info) {	
-	
-	// subprojects 
-	lazy val scalate_fork = project("scalate-fork", "scalate-fork", new ScalateFork(_))
-	lazy val lifty_engine = project("lifty-engine", "lifty-engine", new LiftyEngine(_), scalate_fork)
-	
-	class ScalateFork(info: ProjectInfo) extends DefaultProject(info) {
-    
+class SimpleBuildToolTemplateEngine(info: ProjectInfo) extends ParentProject(info) {  
+  
+  // subprojects 
+  lazy val scalate_fork = project("scalate-fork", "scalate-fork", new ScalateFork(_))
+  lazy val lifty_engine = project("lifty-engine", "lifty-engine", new LiftyEngine(_), scalate_fork)
+  
+  class ScalateFork(info: ProjectInfo) extends DefaultProject(info) {  
     /*
       As long as SBT isn't able to use Scala 2.8.x this project is needed. It's a patched
       version of Scalate that compiles under Scala 2.7.7. 
-  	*/
+    */
+
+    override def version = OpaqueVersion("1.1")
     
     lazy val maven  = MavenRepository("Maven Central Repo","http://repo1.maven.org/maven2/")
     lazy val javam2 = MavenRepository("java.net Maven 2 Repo", "http://download.java.net/maven/2")
@@ -26,30 +27,30 @@ class SimpleBuildToolTemplateEngine(info: ProjectInfo) extends ParentProject(inf
     ) ++ super.libraryDependencies
     
     override def unmanagedClasspath = super.unmanagedClasspath +++ 
-  		(Path.fromFile(buildScalaInstance.compilerJar)) +++ 
-  		(Path.fromFile(buildScalaInstance.libraryJar.getPath))
+      (Path.fromFile(buildScalaInstance.compilerJar)) +++ 
+      (Path.fromFile(buildScalaInstance.libraryJar.getPath))
     
     override def managedStyle = ManagedStyle.Maven
     val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
     Credentials(Path.userHome / "dev" / ".nexus_credentials", log)
   }
 
-  class LiftyEngine(info: ProjectInfo) extends ProcessorProject(info) {	
-    
+  class LiftyEngine(info: ProjectInfo) extends ProcessorProject(info) { 
+        
     override def libraryDependencies = Set(
       "net.liftweb" % "lift-common" % "2.0-M5" % "compile->default",
       "org.scalatest" % "scalatest" % "1.0" % "test"
     ) ++ super.libraryDependencies
 
     override def unmanagedClasspath = super.unmanagedClasspath +++ 
-  		(Path.fromFile(buildScalaInstance.compilerJar)) +++ 
-  		(Path.fromFile(buildScalaInstance.libraryJar.getPath))
+      (Path.fromFile(buildScalaInstance.compilerJar)) +++ 
+      (Path.fromFile(buildScalaInstance.libraryJar.getPath))
 
-  	override def compileOptions = super.compileOptions ++
-  	    Seq("-unchecked","-encoding", "utf8").map(x => CompileOption(x))
+    override def compileOptions = super.compileOptions ++
+        Seq("-unchecked","-encoding", "utf8").map(x => CompileOption(x))
 
     override def managedStyle = ManagedStyle.Maven
     val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
-  	Credentials(Path.userHome / "dev" / ".nexus_credentials", log)
+    Credentials(Path.userHome / "dev" / ".nexus_credentials", log)
   }
 }
