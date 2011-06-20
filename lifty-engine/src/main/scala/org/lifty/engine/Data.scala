d849a8bc477174a310563a632f3860067d5cc871
Mads Hartmann <mads379@gmail.com>
2011-06-20 10:39:13 +0200

    A Descriptor should also list of its template sources.. So I can download them ;)

--- lifty-engine/src/main/scala/org/lifty/engine/Data.scala
+++ lifty-engine/src/main/scala/org/lifty/engine/Data.scala
@@ -4,27 +4,22 @@ package org.lifty.engine
   The following are the data classes that are used internally
 */
 
-trait  Command                                { val keyword: String }
-object HelpCommand            extends Command { val keyword = "help"}
-object CreateCommand          extends Command { val keyword = "create"}
-object TemplatesCommand       extends Command { val keyword = "templates"}
+trait Command { val keyword: String }
+object HelpCommand extends Command { val keyword = "help"}                       
+object CreateCommand extends Command { val keyword = "create"}                   
+object TemplatesCommand extends Command { val keyword = "templates"}             
 object UpdateTemplatesCommand extends Command { val keyword = "update-templates"}
 
-case class Error(
-  message:          String)
+case class Error(message: String)
 
-case class Environment(
-  template:         Template,
-  values:           Map[String,String])
+case class Environment(template: Template, values: Map[String,String])
 
 /*
   The following are the case classes needed to describe a Lifty-engine application
   in JSON
 */
 
-case class Description(
-  repository:     String,
-  templates:      List[Template])
+case class Description(templates: List[Template],sources: List[Source])
 
 case class Template(
   name:           String,
@@ -34,6 +29,11 @@ case class Template(
   files:          List[TemplateFile],
   injections:     List[TemplateInjection],
   dependencies:   List[TemplateReference])
+
+case class Source(
+  name:           String, 
+  url:            String
+)
 
 case class TemplateFile(
   file:           String,   // The scalate template to render
